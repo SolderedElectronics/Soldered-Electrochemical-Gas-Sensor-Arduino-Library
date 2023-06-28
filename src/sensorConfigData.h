58f58a4d2bf32393b8141163c2dc46518d9bf88c
Main Push Robot <robot@example.com>
2023-06-28 11:54:17 +0000

    Committing dev changes

--- src/sensorConfigData.h
+++ src/sensorConfigData.h
@@ -26,33 +26,33 @@ struct sensorType
 // Refer to the official LMP91000 datasheet, chapter 7.6 for more info
 
 /**
- * SGX-4NO-250 - Nitric Oxide sensor 
+ * SGX-4NO-250 - Nitric Oxide sensor
  * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10Ohm
  * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: positive, BIAS: 12%
  * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
  * nanoAmperesPerPPM: 400
  * tiaGainInKOhms: 120
-*/
+ */
 const sensorType SENSOR_NO = {0x18, 0xB7, 0x03, 400.0F, 120};
 
 /**
- * SGX-4NO2 - Nitrogen Dioxide sensor 
+ * SGX-4NO2 - Nitrogen Dioxide sensor
  * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10Ohm
  * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: negative, BIAS: 0%
  * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
  * nanoAmperesPerPPM: 600
  * tiaGainInKOhms: 120
-*/
+ */
 const sensorType SENSOR_NO2 = {0x18, 0xA0, 0x03, 600.0F, 120};
 
 /**
- * SGX-4SO2 - Sulphur Dioxide sensor 
+ * SGX-4SO2 - Sulphur Dioxide sensor
  * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10Ohm
  * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: negative, BIAS: 0%
  * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
  * nanoAmperesPerPPM: 400
  * tiaGainInKOhms: 120
-*/
+ */
 const sensorType SENSOR_SO2 = {0x18, 0xA0, 0x03, 400.0F, 120};
 
 #endif
