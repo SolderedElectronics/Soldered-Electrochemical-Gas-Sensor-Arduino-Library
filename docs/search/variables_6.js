4d941d66dac44c3d69179b03c685f9657119c92a
Docs Robot <robot@example.com>
2023-08-03 06:57:59 +0000

    Committing documentation changes

--- docs/search/variables_6.js
+++ docs/search/variables_6.js
@@ -1,6 +1,12 @@
 var searchData=
 [
-  ['sensor_5fno_0',['SENSOR_NO',['../sensor_config_data_8h.html#ae2365d6703ea94370cda1c69c836ae4e',1,'sensorConfigData.h']]],
-  ['sensor_5fno2_1',['SENSOR_NO2',['../sensor_config_data_8h.html#ad8f664c8ebc27c25e143057fdfadb5c6',1,'sensorConfigData.h']]],
-  ['sensor_5fso2_2',['SENSOR_SO2',['../sensor_config_data_8h.html#a42e9b5103152c9bd15a6bf701edccd28',1,'sensorConfigData.h']]]
+  ['sensor_5fcl2_0',['SENSOR_CL2',['../sensor_config_data_8h.html#acc51d5df51aaf7ea4ab24cdb970bb272',1,'sensorConfigData.h']]],
+  ['sensor_5fco_1',['SENSOR_CO',['../sensor_config_data_8h.html#a7d2fa02042d642b5d413397fcd3ae6c1',1,'sensorConfigData.h']]],
+  ['sensor_5fh2s_2',['SENSOR_H2S',['../sensor_config_data_8h.html#a83138ed8145704cdceb8e27686a92036',1,'sensorConfigData.h']]],
+  ['sensor_5fnh3_3',['SENSOR_NH3',['../sensor_config_data_8h.html#a6b157974433e17a1ec92af0da25cb9ea',1,'sensorConfigData.h']]],
+  ['sensor_5fno_4',['SENSOR_NO',['../sensor_config_data_8h.html#ae2365d6703ea94370cda1c69c836ae4e',1,'sensorConfigData.h']]],
+  ['sensor_5fno2_5',['SENSOR_NO2',['../sensor_config_data_8h.html#ad8f664c8ebc27c25e143057fdfadb5c6',1,'sensorConfigData.h']]],
+  ['sensor_5fo2_6',['SENSOR_O2',['../sensor_config_data_8h.html#a80695087254a0fd4cff6d8f2cb61ffb8',1,'sensorConfigData.h']]],
+  ['sensor_5fo3_7',['SENSOR_O3',['../sensor_config_data_8h.html#a85c2dcdee7118af0dab19bfa3fbac5f5',1,'sensorConfigData.h']]],
+  ['sensor_5fso2_8',['SENSOR_SO2',['../sensor_config_data_8h.html#a42e9b5103152c9bd15a6bf701edccd28',1,'sensorConfigData.h']]]
 ];
