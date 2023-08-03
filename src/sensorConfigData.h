eaad5f57caf55e45fdba776e27d53b6a0f2d3a5f
Robert Sorić <robert.soric@gmail.com>
2023-08-03 08:57:40 +0200

    Update sensorConfigData.h

--- src/sensorConfigData.h
+++ src/sensorConfigData.h
@@ -27,7 +27,7 @@ struct sensorType
 
 /**
  * SGX-4NO-250 - Nitric Oxide sensor
- * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10Ohm
+ * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10 Ohm
  * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: positive, BIAS: 12%
  * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
  * nanoAmperesPerPPM: 400
@@ -37,7 +37,7 @@ const sensorType SENSOR_NO = {0x18, 0xB7, 0x03, 400.0F, 120};
 
 /**
  * SGX-4NO2 - Nitrogen Dioxide sensor
- * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10Ohm
+ * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10 Ohm
  * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: negative, BIAS: 0%
  * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
  * nanoAmperesPerPPM: 600
@@ -46,8 +46,68 @@ const sensorType SENSOR_NO = {0x18, 0xB7, 0x03, 400.0F, 120};
 const sensorType SENSOR_NO2 = {0x18, 0xA0, 0x03, 600.0F, 120};
 
 /**
+ * SGX-4CO - Carbon Monoxide sensor
+ * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10 Ohm
+ * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: negative, BIAS: 0%
+ * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
+ * nanoAmperesPerPPM: 75
+ * tiaGainInKOhms: 120
+ */
+const sensorType SENSOR_CO = {0x18, 0xA0, 0x03, 75.0F, 120};
+
+/**
+ * SGX-4OX - Oxygen sensor
+ * tiacn: TIA_GAIN: 120kOhm, RLOAD: 100 Ohm
+ * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: negative, BIAS: 0%
+ * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
+ * nanoAmperesPerPPM: 75
+ * tiaGainInKOhms: 120
+ */
+const sensorType SENSOR_O2 = {0x1B, 0xA0, 0x03, 75.0F, 120};
+
+/**
+ * PS4-O3-5 - Ozone sensor
+ * tiacn: TIA_GAIN: 120kOhm, RLOAD: 100 Ohm
+ * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: negative, BIAS: 0%
+ * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
+ * nanoAmperesPerPPM: 60
+ * tiaGainInKOhms: 120
+ */
+const sensorType SENSOR_O3 = {0x1B, 0xA0, 0x03, 60.0F, 120};
+
+/**
+ * SGX-4CL2 - Chlorine sensor
+ * tiacn: TIA_GAIN: 120kOhm, RLOAD: 33 Ohm
+ * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: negative, BIAS: 0%
+ * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
+ * nanoAmperesPerPPM: 600
+ * tiaGainInKOhms: 120
+ */
+const sensorType SENSOR_CL2 = {0x19, 0xA0, 0x03, 600.0F, 120};
+
+/**
+ * SGX-4NH3-100 - Ammonia sensor
+ * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10 Ohm
+ * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: negative, BIAS: 0%
+ * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
+ * nanoAmperesPerPPM: 100
+ * tiaGainInKOhms: 120
+ */
+const sensorType SENSOR_NH3 = {0x18, 0xA0, 0x03, 100.0F, 120};
+
+/**
+ * SGX-4DT - Hydrogen Sulfide (and CO) sensor
+ * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10 Ohm
+ * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: negative, BIAS: 0%
+ * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
+ * nanoAmperesPerPPM: 775
+ * tiaGainInKOhms: 120
+ */
+const sensorType SENSOR_H2S = {0x18, 0xA0, 0x03, 775.0F, 120};
+
+/**
  * SGX-4SO2 - Sulphur Dioxide sensor
- * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10Ohm
+ * tiacn: TIA_GAIN: 120kOhm, RLOAD: 10 Ohm
  * refcn: REF_SOURCE: external, INT_Z: 50%, BIAS_SIGN: negative, BIAS: 0%
  * modecn: FET_SHORT: disabled, OP_MODE: 3-lead amperometric cell
  * nanoAmperesPerPPM: 400
