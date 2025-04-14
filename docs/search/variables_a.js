edb006ee319dab80fdef372fd959a7c44aec037e
Docs Robot <robot@example.com>
2025-04-14 07:11:47 +0000

    Committing documentation changes

--- docs/search/variables_a.js
+++ docs/search/variables_a.js
@@ -1,10 +1,11 @@
 var searchData=
 [
-  ['sensor_5fco_0',['SENSOR_CO',['../sensor_config_data_8h.html#a7d2fa02042d642b5d413397fcd3ae6c1',1,'sensorConfigData.h']]],
-  ['sensor_5fh2s_1',['SENSOR_H2S',['../sensor_config_data_8h.html#a83138ed8145704cdceb8e27686a92036',1,'sensorConfigData.h']]],
-  ['sensor_5fnh3_2',['SENSOR_NH3',['../sensor_config_data_8h.html#a6b157974433e17a1ec92af0da25cb9ea',1,'sensorConfigData.h']]],
-  ['sensor_5fno_3',['SENSOR_NO',['../sensor_config_data_8h.html#ae2365d6703ea94370cda1c69c836ae4e',1,'sensorConfigData.h']]],
-  ['sensor_5fno2_4',['SENSOR_NO2',['../sensor_config_data_8h.html#ad8f664c8ebc27c25e143057fdfadb5c6',1,'sensorConfigData.h']]],
-  ['sensor_5fo3_5',['SENSOR_O3',['../sensor_config_data_8h.html#a85c2dcdee7118af0dab19bfa3fbac5f5',1,'sensorConfigData.h']]],
-  ['sensor_5fso2_6',['SENSOR_SO2',['../sensor_config_data_8h.html#a42e9b5103152c9bd15a6bf701edccd28',1,'sensorConfigData.h']]]
+  ['sensor_5fcl2_0',['SENSOR_CL2',['../sensor_config_data_8h.html#acc51d5df51aaf7ea4ab24cdb970bb272',1,'sensorConfigData.h']]],
+  ['sensor_5fco_1',['SENSOR_CO',['../sensor_config_data_8h.html#a7d2fa02042d642b5d413397fcd3ae6c1',1,'sensorConfigData.h']]],
+  ['sensor_5fh2s_2',['SENSOR_H2S',['../sensor_config_data_8h.html#a83138ed8145704cdceb8e27686a92036',1,'sensorConfigData.h']]],
+  ['sensor_5fnh3_3',['SENSOR_NH3',['../sensor_config_data_8h.html#a6b157974433e17a1ec92af0da25cb9ea',1,'sensorConfigData.h']]],
+  ['sensor_5fno_4',['SENSOR_NO',['../sensor_config_data_8h.html#ae2365d6703ea94370cda1c69c836ae4e',1,'sensorConfigData.h']]],
+  ['sensor_5fno2_5',['SENSOR_NO2',['../sensor_config_data_8h.html#ad8f664c8ebc27c25e143057fdfadb5c6',1,'sensorConfigData.h']]],
+  ['sensor_5fo3_6',['SENSOR_O3',['../sensor_config_data_8h.html#a85c2dcdee7118af0dab19bfa3fbac5f5',1,'sensorConfigData.h']]],
+  ['sensor_5fso2_7',['SENSOR_SO2',['../sensor_config_data_8h.html#a42e9b5103152c9bd15a6bf701edccd28',1,'sensorConfigData.h']]]
 ];
--- docs/sensor_config_data_8h.js
+++ docs/sensor_config_data_8h.js
@@ -1,6 +1,7 @@
 var sensor_config_data_8h =
 [
     [ "sensorType", "structsensor_type.html", "structsensor_type" ],
+    [ "SENSOR_CL2", "sensor_config_data_8h.html#acc51d5df51aaf7ea4ab24cdb970bb272", null ],
     [ "SENSOR_CO", "sensor_config_data_8h.html#a7d2fa02042d642b5d413397fcd3ae6c1", null ],
     [ "SENSOR_H2S", "sensor_config_data_8h.html#a83138ed8145704cdceb8e27686a92036", null ],
     [ "SENSOR_NH3", "sensor_config_data_8h.html#a6b157974433e17a1ec92af0da25cb9ea", null ],
